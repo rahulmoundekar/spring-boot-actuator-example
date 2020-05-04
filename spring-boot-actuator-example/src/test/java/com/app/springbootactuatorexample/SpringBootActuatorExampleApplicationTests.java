9015e53fe29d1caf2b01291c019ebfd815cb651f
rahul <rahulmoundekar44@gmail.com>
2020-05-04 11:59:17 +0530

    spring-boot-actuator-example

---
+++ spring-boot-actuator-example/src/test/java/com/app/springbootactuatorexample/SpringBootActuatorExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.springbootactuatorexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootActuatorExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
